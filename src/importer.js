1c6e0ccfcfa4e69a46694adce2759f5959ab2e92
Amen Souissi <amensouissi@ecreall.com>
2018-12-07 09:56:46 +0100

    supports the old version of the code block importer

--- src/importer.js
+++ src/importer.js
@@ -109,7 +109,7 @@ export const htmlToBlock = (nodeName, node) => {
       type: Block.UNSTYLED,
       data: {},
     };
-  } else if (nodeName === 'div' && node.className && node.className === 'md-block-code-container') {
+  } else if (['div', 'pre'].includes(nodeName) && node.className === 'md-block-code-container') {
     return {
       type: Block.CODE,
       data: {},
